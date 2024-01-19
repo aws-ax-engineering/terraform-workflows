f26a0559845789430ae94b0230368019a284b902
gowthamshankar99 <gothamsh@amazon.com>
2024-01-19 12:57:42 -0500

    initial commit

---
+++ test/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> 1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      env                                          = var.instance_name
+      cluster                                      = var.instance_name
+      pipeline                                     = "terraform-action"
+    }
+  }
+}
